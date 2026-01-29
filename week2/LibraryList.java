37c8f2753c040343450ec3a57a91b09eb106b082
jwpark97114 <jwpark97114@gmail.com>
2026-01-29 01:07:05 -0800

    Week 2 Mission 1 complete
    All three problems have been added

---
+++ week2/LibraryList.java
@@ -0,0 +1,21 @@
+class Book{
+    private String title;
+    private String author;
+
+    public String getTitle(){
+        return this.title;
+    }
+    public String getAuthor(){
+        return this.author;
+    }
+}
+
+class Library{
+    
+}
+
+public class LibraryList {
+    public static void main(String[] args) {
+
+    }
+}
