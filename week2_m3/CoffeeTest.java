119385842c2ec3472e7805e099d9621fa6cdf08d
JongWoo Park <jongwoopark@JongWoos-MacBook-Pro.local>
2026-02-01 20:47:49 +0900

    Fix Intellij missing track of files
    No idea why but intellij did not commit all the changes so tried fixing it.

--- week2_m3/CoffeeTest.java
+++ week2_m3/CoffeeTest.java
@@ -1,4 +1,82 @@
-package PACKAGE_NAME;
+class Coffee{
+    String name;
 
-public class CoffeTest {
+    public void brewing(){
+        System.out.println(this.name);
+    }
+}
+
+class EtiopiaAmericano extends Coffee{
+
+    public EtiopiaAmericano(){
+        super();
+        super.name = "EtiopiaAmericano";
+    }
+}
+
+class KenyaAmericano extends Coffee{
+    public KenyaAmericano(){
+        super();
+        super.name = "KenyaAmericano";
+    }
+}
+
+class BrazilAmericano extends Coffee{
+    public BrazilAmericano(){
+        super();
+        super.name = "BrazilAmericano";
+    }
+}
+class HawaiiAmericano extends Coffee{
+    public HawaiiAmericano(){
+        super();
+        super.name = "HawaiiAmericano";
+    }
+}
+
+class Latte extends Coffee{
+    public Latte(Coffee cof){
+        super();
+        super.name = cof.name + " Adding Milk ";
+    }
+}
+class Mocha extends Coffee{
+    public Mocha(Coffee c){
+        super();
+        super.name = c.name + " Adding Mocha Syrup ";
+    }
+}
+class WhippedCream extends Coffee{
+    public WhippedCream(Coffee c){
+        super();
+        super.name = c.name + " Adding WhippedCream ";
+    }
+}
+
+public class CoffeeTest {
+
+    public static void main(String[] args) {
+
+        Coffee etiopiaAmericano = new EtiopiaAmericano();
+        etiopiaAmericano.brewing();
+        System.out.println();
+
+        Coffee kenyaAmericano = new KenyaAmericano();
+        kenyaAmericano.brewing();
+        System.out.println();
+
+        Coffee kenyaLatte = new Latte(kenyaAmericano);
+        kenyaLatte.brewing();
+        System.out.println();
+
+        Coffee brazilMocha = new Mocha(new Latte(new BrazilAmericano()));
+        brazilMocha.brewing();
+        System.out.println();
+
+        Coffee hawaiiWhippedMocha =
+                new WhippedCream(new Mocha(new Latte( new HawaiiAmericano())));
+        hawaiiWhippedMocha.brewing();
+        System.out.println();
+
+    }
 }
--- week2_m3/N1.java
+++ week2_m3/N1.java
@@ -1,4 +1,63 @@
-package PACKAGE_NAME;
+class Member{
+    String name;
+    float discount = 0;
+    float point = 0;
+    int purchaseSum;
+    int hoursStayed;
+    int parkingFee = 3000;
+
+    public Member(String n, int p, int h){
+        this.name = n;
+        this.purchaseSum =p;
+        this.hoursStayed = h;
+    }
+
+    public void print(){
+        System.out.printf(this.name + "님의 지불 금액은 %d 원이고, 적립 포인트는 %d점 입니다.\n",(int)(this.purchaseSum - (this.purchaseSum * this.discount)),(int)(this.purchaseSum*this.point));
+        System.out.printf("주차 요금은 %d 원 입니다.\n", this.parkingFee * this.hoursStayed);
+    }
+}
+class Red extends Member{
+    public Red(String n, int p, int h){
+        super(n,p,h);
+        super.point = 0.0f;
+
+    }
+}
+
+class Platinum extends Member{
+    public Platinum(String n, int p, int h){
+        super(n,p,h);
+        super.discount = 0.05f;
+        super.parkingFee = 1000;
+        super.point = 0.05f;
+
+    }
+}
+
+class Diamond extends Member{
+    public Diamond(String n, int p, int h){
+        super(n,p,h);
+        super.discount = 0.1f;
+        super.parkingFee = 0;
+        super.point = 0.1f;
+    }
+}
 
 public class N1 {
+    public static void main(String[] args) {
+        Member james = new Red("James",10000,2);
+        Member tomas = new Red("Tomas",30000,2);
+        Member ed = new Platinum("Edward",10000,2);
+        Member percy = new Platinum("Percy",30000,2);
+        Member eli = new Diamond("Elizabet",30000,2);
+
+        james.print();
+        tomas.print();
+        ed.print();
+        percy.print();
+        eli.print();
+
+    }
+
 }
