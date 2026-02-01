853f93d4b54c9c1f7bca0647cd4ecb401a22bcc4
JongWoo Park <jongwoopark@JongWoos-MacBook-Pro.local>
2026-02-01 20:48:48 +0900

    Fix missing Mission 2
    Pushing in Intellij is not working as intended trying to fix it.

--- week2_m2/N2.java
+++ week2_m2/N2.java
@@ -1,18 +1,92 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 class Student{
     String name;
     String studentID;
     String major;
-    int korean;
-    int math;
-    int english;
+    int korean = -1;
+    int math = -1;
+    int english = -1;
+    int subjectCount = 0;
 
-    public Student(String studentName, String studentIDNumber, String majorSubject){
-        this.
+    public Student(String studentName, String studentIDNumber, String majorSubject, int scoreKorean, int scoreMath,int scoreEnglish){
+        this.name = studentName;
+        this.studentID=studentIDNumber;
+        this.major=majorSubject;
+        this.korean=scoreKorean;
+        this.math=scoreMath;
+        this.english=scoreEnglish;
+
+        if (this.korean != -1){
+            this.subjectCount++;
+        }
+        if (this.math != -1){
+            this.subjectCount++;
+        }
+        if (this.english != -1){
+            this.subjectCount++;
+        }
     }
+
+    private int scoreSum(){
+        int sum = 0;
+        if (this.korean != -1) {
+            sum += this.korean;
+        }
+        if (this.math != -1) {
+            sum += this.math;
+        }
+        if (this.english != -1) {
+            sum += this.english;
+        }
+        return sum;
+    }
+
+    public void printReport(){
+
+        System.out.printf(this.name + " 학생은 %d 과목을 수강했습니다.\n",this.subjectCount);
+        System.out.printf("총점은 %d 점이고 평균은 %.2f 점입니다.\n", this.scoreSum(), this.scoreSum() / (float)this.subjectCount);
+
+    }
+}
+
+class School{
+    ArrayList<Student> students = new ArrayList<Student>();
+
+    public School(String dir){
+        try(BufferedReader studentLoader = new BufferedReader(new FileReader(dir + "input.txt"))){
+            String tmp;
+            while((tmp = studentLoader.readLine()) != null){
+                String[] studentInfo = tmp.split(",");
+                String name = studentInfo[0];
+                String id = studentInfo[1];
+                String major = studentInfo[2];
+                int[] scores = {-1,-1,-1};
+                for(int i =0; i < studentInfo.length-3; i ++){
+                    scores[i] = Integer.parseInt(studentInfo[3+i]);
+                }
+                students.add(new Student(name,id,major,scores[0],scores[1],scores[2]));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void studentReports(){
+        for(Student student : this.students){
+            student.printReport();
+        }
+    }
+
+
 }
 
 public class N2 {
     public static void main(String[] args) {
-
+        School newSchool = new School("");
+        newSchool.studentReports();
+        System.out.println("");
     }
 }
--- week2_m2/N3.java
+++ week2_m2/N3.java
@@ -1,2 +1,122 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+class State{
+    int money;
+    int[] fruits;
+
+    public State(){
+        this.money = 1000;
+        this.fruits = new int[] {0,0,0};
+    }
+
+    public State(State st){
+        this.money = st.money;
+        this.fruits = st.fruits.clone();
+    }
+}
+
+class Gamer{
+    State state = new State();
+    Memento mem;
+
+    public void addHundredMoney(){
+        this.state.money += 100;
+    }
+
+    public void halfMoney(){
+        this.state.money = this.state.money / 2;
+    }
+
+    public void addFruit(String fruit){
+        int idx = switch (fruit){
+            case "Apple" -> 0;
+            case "Shine Muscat" ->1;
+            default -> 2;
+        };
+
+        this.state.fruits[idx] += 1;
+    }
+
+    public int currentMoney(){
+        return this.state.money;
+    }
+
+    public Memento createMemento(){
+        return new Memento(this.state);
+    }
+
+    public void restoreMemento(Memento m){
+        this.state = new State(m.getState());
+    }
+
+    public boolean shouldRestore(Memento m){
+        if (this.state.money < (m.getState().money / 2)){
+            return true;
+        }
+        return false;
+    }
+
+    public void printState(){
+        System.out.println("Current Money : "+ this.state.money);
+        System.out.println("Fruit Count : "+ "Apple : "+ this.state.fruits[0] + ", Shine Muscat : "+this.state.fruits[1] + ", Tangerine : "+this.state.fruits[2]);
+    }
+
+}
+
+class Memento{
+
+    State savedState;
+
+    public Memento(State state){
+        this.savedState = new State(state);
+    }
+
+    public State getState(){
+        return this.savedState;
+    }
+
+    public void setState(State s){
+        this.savedState = new State(s);
+    }
+
+}
+
+
 public class N3 {
+    public static void main(String[] args) {
+        String[] fruits = {"Apple","Shine Muscat","Tangerine"};
+        int fruitTurn = 0;
+
+        Gamer newGamer = new Gamer();
+        Memento save = newGamer.createMemento();
+
+        while(newGamer.currentMoney() > 0){
+            Random randoDice = new Random();
+            int rolledDice = randoDice.nextInt(1,7);
+            switch(rolledDice){
+                case 1,3:
+                    System.out.println("Gained 100!");
+                    newGamer.addHundredMoney();
+                    save = newGamer.createMemento();
+                    System.out.println("Progress Saved");
+                    break;
+                case 2,4:
+                    System.out.println("Lost half of your money!");
+                    newGamer.halfMoney();
+                    if(newGamer.shouldRestore(save)){
+                        newGamer.restoreMemento(save);
+                        System.out.println("Restored Saved profile!");
+                    }
+                    break;
+                default:
+                    System.out.println("Gained a fruit : " + fruits[fruitTurn]);
+                    newGamer.addFruit(fruits[fruitTurn]);
+                    fruitTurn = (fruitTurn + 1) % 3;
+
+            }
+            newGamer.printState();
+        }
+
+    }
 }
